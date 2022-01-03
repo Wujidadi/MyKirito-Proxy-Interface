a74cd7a66ac2709c5c65233a5f54f9f9cec04ddf
Wujidadi <wujidadi@gmail.com>
2022-01-03 18:58:28 +0800

    實作頂部導航列

--- src/resources/js/components.js
+++ src/resources/js/components.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 
+import navbar from '@/js/components/forestage/navbar.vue';
+
 const components = {
-    //
+    navbar: navbar,
 };
 
 export default components;
--- src/resources/views/forestage/home/index.blade.php
+++ src/resources/views/forestage/home/index.blade.php
@@ -6,8 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>我的桐人 多帳戶代理介面</title>
 </head>
-<body>
-    <div id="app"></div>
+<body class="dark">
+    <div id="app">
+        <navbar></navbar>
+    </div>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </body>
 </html>
