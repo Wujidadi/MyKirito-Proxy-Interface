8d43b597aca8741f6362fccad1144da1f8331dbd
Wujidadi <wujidadi@gmail.com>
2023-01-02 19:36:49 +0800

    更新入口視圖

---
+++ src/resources/views/forestage/home/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>我的桐人多帳戶代理介面</title>
+</head>
+<body>
+    <div id="app">App</div>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</body>
+</html>
