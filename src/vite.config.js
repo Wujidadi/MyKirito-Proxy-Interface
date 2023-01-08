28b060fd74ea2ebd6b955b33c7f29f7a8f0f64ec
Wujidadi <wujidadi@gmail.com>
2023-01-08 15:49:14 +0800

    引入 Vite SVG loader

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
     plugins: [
@@ -16,6 +17,7 @@ export default defineConfig({
                 },
             },
         }),
+        svgLoader(),
     ],
     resolve: {
         alias: {
