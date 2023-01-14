4ff3ad706eca3002ced0da68e26cc1322cfa51ed
Wujidadi <wujidadi@gmail.com>
2023-01-14 17:50:48 +0800

    在主頁面 `<html>` 標籤宣告 `data-bs-theme` 屬性

--- src/resources/views/forestage/home/index.blade.php
+++ src/resources/views/forestage/home/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="zh-Hant-TW">
+<html lang="zh-Hant-TW" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
