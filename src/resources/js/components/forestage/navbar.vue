1e6c91d297da46f2f870e472ba842928684288b8
Wujidadi <wujidadi@gmail.com>
2023-01-14 17:50:05 +0800

    進一步將各頁面的樣式拆分改寫到 vue 組件檔案中

--- src/resources/js/components/forestage/navbar.vue
+++ src/resources/js/components/forestage/navbar.vue
@@ -8,20 +8,70 @@
         <router-link class="nav-link" to="/hall-of-fame">名人堂</router-link>
         <router-link class="nav-link" to="/reincarnation">轉生</router-link>
         <router-link class="nav-link" to="/reports">戰報</router-link>
-        <a class="nav-link final"><theme-switcher style="width: 28px;" /></a>
+        <a class="nav-link final">
+            <theme-switcher style="width: 28px;"/>
+        </a>
     </nav>
 </template>
 
 <script>
-    import ThemeSwitcher from '@/assets/icons/theme-switcher.svg';
-    export default {
-        name: "Navbar",
-        components: {
-            ThemeSwitcher,
-        },
-    };
+import ThemeSwitcher from '@/assets/icons/theme-switcher.svg';
+
+export default {
+    name: "Navbar",
+    components: {
+        ThemeSwitcher,
+    },
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import '@/sass/_variables.scss';
 
+nav.navbar {
+    position: fixed;
+    top: 0;
+    z-index: 999;
+    width: 100%;
+    padding: 0 5%;
+    margin-bottom: 18px;
+    background-color: $primary-bg-color;
+    -webkit-box-align: center;
+    align-items: center;
+    box: {
+        sizing: border-box;
+        shadow: rgb(0 0 0 / 30%) 0 2px 4px;
+    }
+
+    a.nav-link {
+        // background-color: $primary-bg-color;
+        color: $primary-fg-color;
+        display: flex;
+        -webkit-box-align: center;
+        align-items: center;
+        height: $navbar-height;
+        padding: 0 8px;
+        transition: all 0.16s ease 0s;
+
+        &.final {
+            margin-left: auto;
+            cursor: pointer;
+        }
+
+        &:hover {
+            text-decoration: none;
+        }
+
+        &:not(.router-link-active) {
+            &:hover {
+                background-color: transparentize($primary-fg-color, 0.8);
+            }
+        }
+
+        &.router-link-active {
+            background-color: $primary-fg-color;
+            color: $primary-bg-color;
+        }
+    }
+}
 </style>
