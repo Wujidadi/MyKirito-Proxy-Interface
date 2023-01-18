8a4610cd99557faa5548b484036db448badf86ba
Wujidadi <wujidadi@gmail.com>
2023-01-18 22:56:46 +0800

    新增 `JwtMiddleware` (#1, #3)

---
+++ src/app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Constants\ErrDef;
+use App\Traits\JsonResponseBuilder;
+use Closure;
+use Exception;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JwtMiddleware
+{
+    use JsonResponseBuilder;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+            if (!$user) {
+                return $this->singleJsonResponse(ErrDef::LOGIN_NOT_MATCH, '帳號或密碼錯誤', Response::HTTP_UNAUTHORIZED);
+            }
+        } catch (JWTException $e) {
+            return $this->singleJsonResponse(ErrDef::JWT_EXCEPTION, $e->getMessage(), Response::HTTP_UNAUTHORIZED);
+        } catch (Exception $e) {
+            return $this->singleJsonResponse(ErrDef::GENERAL_ERROR, $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return $next($request);
+    }
+}
