d02601c6b600681847def7b3df21861013dd2e93
Wujidadi <wujidadi@gmail.com>
2023-01-21 13:05:46 +0800

    引入自訂的 Widgets library

---
+++ src/widgets/Color.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Widget;
+
+/**
+ * 顏色名稱與 16 進位色碼定義映射表
+ */
+class Color
+{
+    # 粉紅色系
+    public const MediumVioletRed = '#c71585';
+    public const DeepPink = '#ff1493';
+    public const PaleVioletRed = '#db7093';
+    public const HotPink = '#ff69b4';
+    public const LightPink = '#ffb6c1';
+    public const Pink = '#ffc0cb';
+
+    # 紅色系
+    public const DarkRed = '#8b0000';
+    public const Red = '#ff0000';
+    public const Firebrick = '#b22222';
+    public const Crimson = '#dc143c';
+    public const IndianRed = '#cd5c5c';
+    public const LightCoral = '#f08080';
+    public const Salmon = '#fa8072';
+    public const DarkSalmon = '#e9967a';
+    public const LightSalmon = '#ffa07a';
+
+    # 橘色系
+    public const OrangeRed = '#ff4500';
+    public const Tomato = '#ff6347';
+    public const DarkOrange = '#ff8c00';
+    public const Coral = '#ff7f50';
+    public const Orange = '#ffa500';
+
+    # 黃色系
+    public const DarkKhaki = '#bdb76b';
+    public const Gold = '#ffd700';
+    public const Khaki = '#f0e68c';
+    public const PeachPuff = '#ffdab9';
+    public const Yellow = '#ffff00';
+    public const PaleGoldenrod = '#eee8aa';
+    public const Moccasin = '#ffe4b5';
+    public const PapayaWhip = '#ffefd5';
+    public const LightGoldenrodYellow = '#fafad2';
+    public const LemonChiffon = '#fffacd';
+    public const LightYellow = '#ffffe0';
+
+    # 棕色系
+    public const Maroon = '#800000';
+    public const Brown = '#a52a2a';
+    public const SaddleBrown = '#8b4513';
+    public const Sienna = '#a0522d';
+    public const Chocolate = '#d2691e';
+    public const DarkGoldenrod = '#b8860b';
+    public const Peru = '#cd853f';
+    public const RosyBrown = '#bc8f8f';
+    public const Goldenrod = '#daa520';
+    public const SandyBrown = '#f4a460';
+    public const Tan = '#d2b48c';
+    public const Burlywood = '#deb887';
+    public const Wheat = '#f5deb3';
+    public const NavajoWhite = '#ffdead';
+    public const Bisque = '#ffe4c4';
+    public const BlanchedAlmond = '#ffebcd';
+    public const Cornsilk = '#fff8dc';
+
+    # 綠色系
+    public const DarkGreen = '#006400';
+    public const Green = '#008000';
+    public const DarkOliveGreen = '#556b2f';
+    public const ForestGreen = '#228b22';
+    public const SeaGreen = '#2e8b57';
+    public const Olive = '#808000';
+    public const OliveDrab = '#6b8e23';
+    public const MediumSeaGreen = '#3cb371';
+    public const LimeGreen = '#32cd32';
+    public const Lime = '#00ff00';
+    public const SpringGreen = '#00ff7f';
+    public const MediumSpringGreen = '#00fa9a';
+    public const DarkSeaGreen = '#8fbc8f';
+    public const MediumAquamarine = '#66cdaa';
+    public const YellowGreen = '#9acd32';
+    public const LawnGreen = '#7cfc00';
+    public const Chartreuse = '#7fff00';
+    public const LightGreen = '#90ee90';
+    public const GreenYellow = '#adff2f';
+    public const PaleGreen = '#98fb98';
+
+    # 青色系
+    public const Teal = '#008080';
+    public const DarkCyan = '#008b8b';
+    public const LightSeaGreen = '#20b2aa';
+    public const CadetBlue = '#5f9ea0';
+    public const DarkTurquoise = '#00ced1';
+    public const MediumTurquoise = '#48d1cc';
+    public const Turquoise = '#40e0d0';
+    public const Aqua = '#00ffff';
+    public const Cyan = '#00ffff';
+    public const Aquamarine = '#7fffd4';
+    public const PaleTurquoise = '#afeeee';
+    public const LightCyan = '#e0ffff';
+
+    # 藍色系
+    public const Navy = '#000080';
+    public const DarkBlue = '#00008b';
+    public const MediumBlue = '#0000cd';
+    public const Blue = '#0000ff';
+    public const MidnightBlue = '#191970';
+    public const RoyalBlue = '#4169e1';
+    public const SteelBlue = '#4682b4';
+    public const DodgerBlue = '#1e90ff';
+    public const DeepSkyBlue = '#00bfff';
+    public const CornflowerBlue = '#6495ed';
+    public const SkyBlue = '#87ceeb';
+    public const LightSkyBlue = '#87cefa';
+    public const LightSteelBlue = '#b0c4de';
+    public const LightBlue = '#add8e6';
+    public const PowderBlue = '#b0e0e6';
+
+    # 紫色系
+    public const Indigo = '#4b0082';
+    public const Purple = '#800080';
+    public const DarkMagenta = '#8b008b';
+    public const DarkViolet = '#9400d3';
+    public const DarkSlateBlue = '#483d8b';
+    public const RebeccaPurple = '#663399';
+    public const BlueViolet = '#8a2be2';
+    public const DarkOrchid = '#9932cc';
+    public const Fuchsia = '#ff00ff';
+    public const Magenta = '#ff00ff';
+    public const SlateBlue = '#6a5acd';
+    public const MediumSlateBlue = '#7b68ee';
+    public const MediumOrchid = '#ba55d3';
+    public const MediumPurple = '#9370db';
+    public const Orchid = '#da70d6';
+    public const Violet = '#ee82ee';
+    public const Plum = '#dda0dd';
+    public const Thistle = '#d8bfd8';
+    public const Lavender = '#e6e6fa';
+
+    # 白色系
+    public const MistyRose = '#ffe4e1';
+    public const AntiqueWhite = '#faebd7';
+    public const Linen = '#faf0e6';
+    public const Beige = '#f5f5dc';
+    public const WhiteSmoke = '#f5f5f5';
+    public const LavenderBlush = '#fff0f5';
+    public const OldLace = '#fdf5e6';
+    public const AliceBlue = '#f0f8ff';
+    public const Seashell = '#fff5ee';
+    public const GhostWhite = '#f8f8ff';
+    public const Honeydew = '#f0fff0';
+    public const FloralWhite = '#fffaf0';
+    public const Azure = '#f0ffff';
+    public const MintCream = '#f5fffa';
+    public const Snow = '#fffafa';
+    public const Ivory = '#fffff0';
+    public const White = '#ffffff';
+
+    # 灰黑色系
+    public const Black = '#000000';
+    public const DarkSlateGray = '#2f4f4f';
+    public const DimGray = '#696969';
+    public const SlateGray = '#708090';
+    public const Gray = '#808080';
+    public const LightSlateGray = '#778899';
+    public const DarkGray = '#a9a9a9';
+    public const Silver = '#c0c0c0';
+    public const LightGray = '#d3d3d3';
+    public const Gainsboro = '#dcdcdc';
+}
